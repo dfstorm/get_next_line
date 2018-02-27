3362b36e0e94bc8a0f3f0412a77502ce83680aa6
Gabriel Genois <ggenois@z2r6p1.le-101.fr>
2018-02-27 23:22:23 +0100

    oh yeah

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ggenois <ggenois@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/02/27 14:57:11 by ggenois      #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/27 17:15:35 by ggenois     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/02/27 23:20:41 by ggenois     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,10 +19,11 @@ int		ft_finditem(char *haystack, char item)
 	int	i;
 
 	i = -1;
-	while (haystack[++i] != '\0')
-		if (haystack[i] == item)
-			return (i);
-	return (0);
+	if (haystack)
+		while (haystack[++i])
+			if (haystack[i] == item)
+				return (i);
+	return (-1);
 }
 
 char	*ft_readfile_once(int fd)
@@ -31,58 +32,100 @@ char	*ft_readfile_once(int fd)
 	char	cbuffer[BUFF_SIZE];
 	char	*str;
 
-	if (BUFF_SIZE > 0)
-		if ((str = ft_memalloc(BUFF_SIZE + 1)) == NULL)
-			return (0);
+	str = ft_memalloc(1);
+	if (BUFF_SIZE < 1 || str == NULL)
+		return (0);
 	while ((i = read(fd, cbuffer, BUFF_SIZE)) > 0)
 	{
-		str = ft_strcpy(str, cbuffer);
+		if ((str = ft_memalloc(i + 1)) == NULL)
+			return (0);
+		str = ft_strncpy(str, cbuffer, i);
 		break ;
 	}
 	return (str);
 }
 
+void	doclnup(char **tmp, char **g_bread, int *i)
+{
+	(*tmp) = NULL;
+	free(*tmp);
+	(*g_bread) = NULL;
+	free((*g_bread));
+	(*i) = 0;
+	free(i);
+}
+
+char	*ft_strjoin_patched(char *s1, char *s2)
+{
+	int		x;
+	char	*sx;
+
+	x = ft_strlen(s1) + ft_strlen(s2) + 1;
+	sx = ft_memalloc(x);
+	x = 0;
+	while (s1[x] != '\0')
+	{
+		sx[x] = s1[x];
+		x++;
+	}
+	while (s2[x - ft_strlen(s1)] != '\0')
+	{
+		sx[x] = s2[x - ft_strlen(s1)];
+		x++;
+	}
+	sx[x] = '\0';
+	return (sx);
+}
+
 int	get_next_line(const int fd, char **line)
 {
-	static int		g_ipos = 0;
-	static t_data	g_data;
-	char			*str;
+	static char		*g_bread = "";
 	char			*tmp;
-	int 			i;
+	int				i;
+	static int		istop = 0;
 
-	str = ft_memalloc(1);
-	// 0- Init du tableau.
-	if(g_ipos == 0)
+	if (istop == 1)
+		return (0);
+
+	i = ft_finditem(g_bread, '\n');
+	if (i >= 0)
 	{
-		g_data.str = ft_memalloc(1);
-		g_ipos++;
+		if (i == 0)
+			(*line) = ft_memalloc(1);
+		else
+			(*line) = ft_strdup(ft_strsplit(g_bread, '\n')[0]);
+		g_bread = ft_strsub(g_bread, i + 1, (ft_strlen(g_bread) - i) - 1);
+		return (1);
 	}
-
-
-	while ((tmp = ft_readfile_once(fd)) != NULL)
+	else
 	{
-		g_data.str = ft_strjoin(g_data.str, tmp);
-		i = ft_finditem(g_data.str, '\n');
-		if(i != 0)
+		while (1)
 		{
-			str = ft_memalloc(i + 1);
-			str = ft_strsub(g_data.str, 0, i);
-			
-			ft_putstr(str);
-			ft_putnbr(i);
-			ft_putnbr(ft_strlen(g_data.str));
-			str = ft_strsub(g_data.str, i, ft_strlen(g_data.str));
-			ft_putstr("--");
-			ft_putstr(str);
-			ft_putstr("--");
-			break ;
+			tmp = ft_readfile_once(fd);
+			if (tmp == 0)
+				return (-1);
+			g_bread = ft_strjoin_patched(g_bread, tmp);
+			i = ft_finditem(g_bread, '\n');
+			if (i >= 0)
+			{
+				if (i == 0)
+					(*line) = ft_memalloc(1);
+				else
+					(*line) = ft_strdup(ft_strsplit(g_bread, '\n')[0]);
+				g_bread = ft_strsub(g_bread, i + 1, (ft_strlen(g_bread) - i) - 1);
+				return (1);
+			}
+			else if (tmp[0] == '\0')
+				(*line) = ft_strsub(g_bread, 1, (ft_strlen(g_bread) - 1));
+
+
+			if (tmp[0] == '\0')
+			{
+				istop = 1;
+				return (1);
+			}
 		}
-
 	}
-
-	(*line) = str;
-
-	ft_putstr("\n=== gnl-end ===\n");
-	
-	return (1);
+	doclnup(&tmp, &g_bread, &i);
+	return (-1);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ggenois <ggenois@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/02/26 10:50:01 by ggenois      #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/27 17:12:34 by ggenois     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/02/27 20:59:26 by ggenois     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -26,18 +26,25 @@ int		main(int argc, char **argv)
 	if (argc > 1)
 	{
 		fd = open(argv[1], O_RDONLY);
-		
+
+		while (get_next_line(fd, &str) == 1)
+		{
+			ft_putstr("from main: "); ft_putstr(str);
+			ft_putstr("\n");
+		}
+		/*
 		get_next_line(fd, &str);
-		ft_putstr(str);
+		ft_putstr("from main: "); ft_putstr(str);
 		ft_putstr("\n");
 		get_next_line(fd, &str);
-		ft_putstr(str);ft_putstr("\n");
+		ft_putstr("from main: "); ft_putstr(str);ft_putstr("\n");
 		get_next_line(fd, &str);
-		ft_putstr(str);ft_putstr("\n");
+		ft_putstr("from main: "); ft_putstr(str);ft_putstr("\n");
 		get_next_line(fd, &str);
-		ft_putstr(str);ft_putstr("\n");
+		ft_putstr("from main: "); ft_putstr(str);ft_putstr("\n");
 		get_next_line(fd, &str);
-		ft_putstr(str);ft_putstr("\n");
+		ft_putstr("from main: "); ft_putstr(str);ft_putstr("\n");
+		*/
 	}
 	ft_putstr("\n");
 	return (0);
